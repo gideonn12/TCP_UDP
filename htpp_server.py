c80f96742a07a8f0ea702b81020228a0d2820efc
Gideon Neeman <gideoneeman@icloud.com>
2024-03-08 17:09:21 +0200

    basic server

---
+++ htpp_server.py
@@ -0,0 +1,18 @@
+from socket import socket, AF_INET, SOCK_STREAM
+# socket creation
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('', 12345))
+s.listen(5)
+while True:
+    # accept connection
+    client, addr = s.accept()
+    # read HTTP request from socket
+    request = client.recv(4096)
+    print(request)
+    # reply to client
+    client.send(b'HTTP/1.1 200 OK\n')
+    client.send(b'Content-Type: text/html\n')
+    client.send(b'Access-Control-Allow-Origin: *\n')
+    client.send(b'\n')
+    client.send(b'Hello world')
+    client.close()
